ff0a05077fd346bc619dcbabbbe9fb9ec114ca04
Rui Zhang <rz992@nyu.edu>
2020-06-06 15:54:45 -0400

    etl pipeline update

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df=messages.merge(categories,on=['id'],how='left')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';',expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+    category_colnames = row.apply(lambda x:x[:-2])
+    categories.columns = category_colnames
+    for column in categories:
+    # set each value to be the last character of the string
+    categories[column] = categories[column].str[-1]
+    # convert column from string to numeric
+    categories[column] = categories[column].astype(int)
+    # drop the original categories column from `df`
+    df=df.drop(columns=['categories'])
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1,join='inner')
+    # check number of duplicates
+    df["is_duplicate"]=df.duplicated(keep='first')
+    # drop duplicates
+    df=df[df["is_duplicate"]==False]
+    df.drop(columns=['is_duplicate'])
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///InsertDatabaseName.db')
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
@@ -24,12 +49,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
